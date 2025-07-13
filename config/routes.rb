615bf8a764754bd06ff1e676dacb6ad9a064a318
Said-MZ <saidzain897@gmail.com>
2025-07-13 10:36:39 +0300

    Add user fixtures and initial user model test
    - Created a new fixture file for users with placeholder entries.
    - Added a basic test file for the User model to establish a testing framework.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
 
   # Defines the root path route ("/")
   # root "posts#index"
 
-  get '/home', to: 'pages#home'
-  get "/about", to: 'pages#about'
+  get "/home", to: "pages#home"
+  get "/about", to: "pages#about"
   root "pages#home"
 end
