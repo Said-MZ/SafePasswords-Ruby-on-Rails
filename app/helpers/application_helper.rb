39e00d816f62def9e33b1cd316b2c9ba38544ab5
Said-MZ <saidzain897@gmail.com>
2025-07-13 11:29:35 +0300

    Refactor navbar and layout; implement logo helper for consistent branding

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def logo(size = "h2")
+    link_to(root_path, class: "logo #{size} text-decoration-none") do
+      content_tag(:h2, "🔒 SafePasswords")
+    end
+  end
 end
